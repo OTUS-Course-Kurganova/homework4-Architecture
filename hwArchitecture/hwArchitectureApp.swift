535ec406fbcdcd38e6f09e54be24815bc155fb1c
Alexandra Kurganova <axndkgn@icloud.com>
2023-05-22 10:51:35 +0300

    Initial Commit

---
+++ hwArchitecture/hwArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  hwArchitectureApp.swift
+//  hwArchitecture
+//
+//  Created by Alexandra Kurganova on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct hwArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
